73495c5b13710a51b83703149c6c2a7671607e43
andresgamez13 <56166736+andresgamez13@users.noreply.github.com>
2020-04-23 11:17:35 -0500

    Update FNC.py

--- FNC.py
+++ FNC.py
@@ -49,10 +49,44 @@ def enFNC(A):
 def Tseitin(A, letrasProposicionalesA):
     letrasProposicionalesB = [chr(x) for x in range(256, 1200)]
     assert(not bool(set(letrasProposicionalesA) & set(letrasProposicionalesB))), u"¡Hay letras proposicionales en común!"
-
-    #  IMPLEMENTAR AQUI ALGORITMO TSEITIN
-
-    pass
+    L=[]
+    pila=[]
+    i=-1
+    s=A[0]
+    while len(A)>0:
+        if (s in letrasProposicionalesA or s in letrasProposicionalesB) and (len(pila)>0) and (pila[-1]=="-"):
+            i+=1
+            atomo=letrasProposicionalesB[i]
+            pila=pila[:-1]
+            pila.append(atomo)
+            L.append(atomo+">"+"-"+s)
+            A=A[1:]
+            if len(A)>0:
+                s=A[0]
+        elif s==")":
+            w=pila[-1]
+            u=pila[-2]
+            v=pila[-3]
+            pila=pila[:len(pila)-4]
+            i+=1
+            atomo=letrasProposicionalesB[i]
+            L.append(atomo+">"+"("+v+u+w+")")
+            s=atomo
+        else:
+            pila.append(s)
+            A=A[1:]
+            if len(A)>0:
+                s=A[0]
+    b=""
+    if i<0:
+        atomo=pila[-1]
+    else:
+        atomo=letrasProposicionalesB[i]
+    for x in L:
+        y=enFNC(x)
+        b=b+"Y"+y
+    b=atomo+b
+    return b
 
 # Subrutina Clausula para obtener lista de literales
 # Input: C (cadena) una clausula
